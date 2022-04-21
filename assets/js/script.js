9ebad40a38dc654f5918d0b7648abdd0400519c2
oliveirabrunati <oliveira_bruna@outlook.com>
2022-04-21 20:34:35 -0300

    verificação palindromo javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,31 @@ function verificaPalindromo() {
 
     let arrayRevertido = palavraFrase.split("").reverse().join("");
 
-    console.log(arrayRevertido);
+    // console.log(arrayRevertido);
 
-    if (palavraFrase != arrayRevertido) {
-        return console.log("Não é um palíndromo.");
-    } else {
-        return console.log("É um palíndromo");
-    }
+    let algunsExemplos = document.getElementById("textoExemplo");
+    let exemplo = document.createElement("span");
+    let palindromo = document.createTextNode(" ");
+
+    algunsExemplos.appendChild(exemplo);
+    exemplo.appendChild(palindromo);
+
+    if (palavraFrase !== arrayRevertido) {
+
+        let palindromo = document.createTextNode("Não é um palíndromo.");
+
+        exemplo.appendChild(palindromo);
+
+    } else (palavraFrase === arrayRevertido)
+        let epalindromo = document.createTextNode("É um palíndromo.");
+
+    exemplo.appendChild(epalindromo);
+    // return console.log("É um palíndromo.");
+
+
 
 }
 
 verificaPalindromo();
+
+
