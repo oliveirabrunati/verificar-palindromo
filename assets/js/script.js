9842acaba60b37320c1ee2db536c05e097088097
oliveirabrunati <oliveira_bruna@outlook.com>
2022-04-21 23:48:21 -0300

    verificando palindromo javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,31 +8,31 @@ function verificaPalindromo() {
 
     let arrayRevertido = palavraFrase.split("").reverse().join("");
 
-    // console.log(arrayRevertido);
-
     let algunsExemplos = document.getElementById("textoExemplo");
     let exemplo = document.createElement("span");
-    let palindromo = document.createTextNode(" ");
+    // let palindromo = document.createTextNode(" ");
+
+    algunsExemplos.innerHTML = "";
 
     algunsExemplos.appendChild(exemplo);
-    exemplo.appendChild(palindromo);
+    // exemplo.appendChild(palindromo);
 
-    if (palavraFrase !== arrayRevertido) {
+    if(palavraFrase !== ""){
+        console.log(typeof(palavraFrase));
+        if (palavraFrase === arrayRevertido) {
+    
+            let palindromo = document.createTextNode("É um palíndromo.");
+    
+            exemplo.appendChild(palindromo);
+        } else {
+            let epalindromo = document.createTextNode("Não é um palíndromo.");
+    
+            exemplo.appendChild(epalindromo);
+        }
+    }
 
-        let palindromo = document.createTextNode("Não é um palíndromo.");
-
-        exemplo.appendChild(palindromo);
-
-    } else (palavraFrase === arrayRevertido)
-        let epalindromo = document.createTextNode("É um palíndromo.");
-
-    exemplo.appendChild(epalindromo);
-    // return console.log("É um palíndromo.");
-
-
+    
 
 }
-
-verificaPalindromo();
 
 
